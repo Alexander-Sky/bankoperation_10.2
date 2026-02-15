d7ed185edfbf7a5a5320aebcc1a00f54ec603f15
Alexander Schischkin <schischkin2@yandex.com>
2026-02-15 19:32:20 +0100

    add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from datetime import datetime
+
+@pytest.fixture
+def sample_operations():
+    return [
+        {
+            'id': 41428829,
+            'state': 'EXECUTED',
+            'date': '2019-07-03T18:35:29.512364',
+            'description': 'Перевод с карты на карту',
+            'operation_id': 56789,
+            'amount': {
+                'amount': 10000,
+                'currency': 'RUB'
+            }
+        },
+        # Другие тестовые операции
+    ]
